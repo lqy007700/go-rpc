3bcacee34db138e490d75b5ea858a5d121b9be0a
lqy007700 <lqy007700@163.com>
2022-05-20 16:31:59 +0800

    -服务注册

--- main/main.go
+++ main/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	gorpc "go-rpc"
-	"go-rpc/codec"
 	"log"
 	"net"
 	"time"
@@ -22,26 +20,46 @@ func startServer(addr chan string) {
 }
 
 func main() {
-	addr := make(chan string)
-	go startServer(addr)
+	quit := make(chan struct{})
+	go checkGoroutine(quit)
 
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { conn.Close() }()
+	time.Sleep(time.Second * 5)
 
-	time.Sleep(time.Second)
+	quit <- struct {}{}
 
-	_ = json.NewEncoder(conn).Encode(gorpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
-	// send request & receive response
-	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
+	//log.SetFlags(0)
+	//addr := make(chan string)
+	//go startServer(addr)
+	//client, _ := gorpc.Dial("tcp", <-addr)
+	//defer func() { _ = client.Close() }()
+	//
+	//time.Sleep(time.Second)
+	//// send request & receive response
+	//var wg sync.WaitGroup
+	//for i := 0; i < 5; i++ {
+	//	wg.Add(1)
+	//	go func(i int) {
+	//		defer wg.Done()
+	//		args := fmt.Sprintf("geerpc req %d", i)
+	//		var reply string
+	//		if err := client.Call("Foo.Sum", args, &reply); err != nil {
+	//			log.Fatal("call Foo.Sum error:", err)
+	//		}
+	//		log.Printf("reply:%+v", reply)
+	//	}(i)
+	//}
+	//wg.Wait()
+}
+
+func checkGoroutine(done chan struct{}) {
+
+	for {
+		select {
+		case <-done:
+			return
+		default:
 		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		time.Sleep(time.Second)
+		fmt.Println(1)
 	}
 }
--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"reflect"
 	"sync"
+	"sync/atomic"
 )
 
 const MagicNumber = 0x3bef5c
@@ -23,8 +24,7 @@ var DefaultOption = &Option{
 	CodecType:   codec.GobType,
 }
 
-type Server struct {
-}
+type Server struct{}
 
 func NewServer() *Server {
 	return &Server{}
@@ -40,7 +40,6 @@ func (s *Server) Accept(lis net.Listener) {
 			return
 		}
 		go s.ServerConn(conn)
-
 	}
 }
 
@@ -148,4 +147,38 @@ func (s *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex
 	log.Println(req.h, req.argv.Elem())
 	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
 	s.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
+
+type methodType struct {
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint64
+}
+
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+func (m *methodType) newArgv() reflect.Value {
+	var argv reflect.Value
+	// arg may be a pointer type, or a value type
+	if m.ArgType.Kind() == reflect.Ptr {
+		argv = reflect.New(m.ArgType.Elem())
+	} else {
+		argv = reflect.New(m.ArgType).Elem()
+	}
+	return argv
+}
+
+func (m *methodType) newReplyv() reflect.Value {
+	// reply must be a pointer type
+	replyv := reflect.New(m.ReplyType.Elem())
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+	}
+	return replyv
 }
